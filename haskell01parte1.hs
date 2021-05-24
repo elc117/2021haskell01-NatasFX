9ab725d2abd9b7dd596802c7c1a6d5f15173b8f1
NatasFX <nataschmitt02@hotmail.com>
2021-05-24 14:49:34 -0300

    finished first part

--- haskell01parte1.hs
+++ haskell01parte1.hs
@@ -1,2 +1,38 @@
+module Test where
 -- Prática 01 de Haskell - Parte 1
--- Nome: 
+-- Nome: Natã Schmitt
+
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x^2 + y^2
+
+circleArea :: Float -> Float
+circleArea r = pi*r^2
+
+age :: Int -> Int -> Int
+age anoNasc anoAtual = anoAtual - anoNasc
+
+isElderly :: Int -> Bool
+isElderly age = age > 65
+
+htmlItem :: String -> String
+-- Em html não é <\li> e sim </li>
+htmlItem str = "<li>" ++ str ++ "</li>"
+
+startsWithA :: String -> Bool
+startsWithA str = str!!0 == 'A'
+
+isVerb :: String -> Bool
+isVerb str = str!!(length str-1) == 'r'
+
+isVowel :: Char -> Bool
+isVowel c = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads listA listB = head listA == head listB
+
+isVowel2 :: Char -> Bool
+isVowel2 c = elem c ['a', 'e', 'i', 'o', 'u']
+
+
+
+
