37bf1e8f6f492ccfcf0fa94d1f2c949c4e5d66db
NatasFX <nataschmitt02@hotmail.com>
2021-05-24 15:39:39 -0300

    done part 2

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,2 +1,33 @@
 -- Prática 01 de Haskell - Parte 2
--- Nome: 
+-- Nome: Natã Schmitt
+
+--Importando funções da parte 1
+import Test
+import Quickstart
+
+itemize :: [String] -> [String]
+itemize list = map htmlItem list
+
+onlyVowels :: String -> String
+onlyVowels str = filter isVowel2 str
+
+onlyElderly :: [Int] -> [Int]
+onlyElderly ages = filter isElderly ages
+
+onlyLongWords :: [String] -> [String]
+onlyLongWords strings = filter isLongWord strings
+
+onlyEven :: [Int] -> [Int]
+onlyEven intList = filter (\n -> mod n 2 == 0) intList
+
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 intList = filter (\n -> n >= 60 && n <= 80) intList
+
+countSpaces :: String -> Int
+countSpaces str = length (filter (' '==) str)
+
+calcAreas :: [Float] -> [Float]
+calcAreas floatList = map circleArea floatList
+
+charFound :: Char -> String -> Bool
+charFound chr str = any (chr==) str
